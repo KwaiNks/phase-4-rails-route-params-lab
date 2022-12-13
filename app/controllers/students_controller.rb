8579592e3915ab01f7ca7c9db5bb26fa027293e6
Richmond Nkansah <richmondnkansah@gmail.com>
2022-12-13 10:43:50 -0500

    commit tree 3838c5e95839b0495a4b9627b9e3a070f5e4d4bf

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.all.find(params[:first_name])
     render json: students
   end
+
+  def student_info
+    info = Student.find(params[:id])
+    render json: info
+  end
+
 
 end
